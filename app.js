6a158d2954ccf6e47b349c3b8451d708d4890d10
Ronald Erosa <thecasualdev@gmail.com>
2021-06-12 15:52:59 +0800

    Removed extra cors config

--- app.js
+++ app.js
@@ -3,16 +3,7 @@ import express from 'express';
 import cors from 'cors';
 var app = express();
 
-var whitelist = ['http://localhost:3001', 'https://opc-ui.netlify.app/']
 var corsOptions = {
-    origin: function (origin, callback) {
-        if (whitelist.indexOf(origin) !== -1) {
-            callback(null, true)
-        } else {
-            callback(new Error('Not allowed by CORS'))
-        }
-    },
-    changeOrigin: true, 
     optionsSuccessStatus: 200
 }
 const port = process.env.PORT || 3000;
